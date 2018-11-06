c6e99babf8adbea4a99b40d14fd34a03eded232a
mohanedabdullha <43791989+mohanedabdullha@users.noreply.github.com>
2018-11-06 19:56:56 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("508619864487886858");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`Wlcome To Server **Magic**`), 4000)        
+}
+});
+bot.login(process.env.BOT_TOKEN)
